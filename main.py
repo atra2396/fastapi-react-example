16837c2358177a07fbaa11ef339e3a57779dd4eb
Alijah Green <atravasc@myriad.com>
2022-03-18 18:50:09 +0000

    Mostly working fastapi-react integration

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="ndb-ui/public")
+templates = Jinja2Templates(directory="ndb-ui/build/")
 
-app.mount("/static", StaticFiles(directory="ndb-ui/public/"), name="static")
+app.mount(
+    "/static", StaticFiles(directory="ndb-ui/build/static"), name="ui-build-assets"
+)
 
 
 @app.get("/")
 def get_site(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+app.mount("/", StaticFiles(directory="ndb-ui/public"), name="public-assets")
